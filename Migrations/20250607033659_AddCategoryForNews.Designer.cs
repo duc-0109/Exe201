3c27fa619d0446b5d5bd9ddf3f917768ed7516e4
traiquanhp <tunmhe171130@fpt.edu.vn>
2025-06-07 11:11:57 +0700

    AddCategoryForNews
    mn nhớ dotnet ef database update

--- Migrations/20250607033659_AddCategoryForNews.Designer.cs
+++ Migrations/20250607033659_AddCategoryForNews.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SmartCookFinal.Models;
 
@@ -11,9 +12,11 @@ using SmartCookFinal.Models;
 namespace SmartCookFinal.Migrations
 {
     [DbContext(typeof(SmartCookContext))]
-    partial class SmartCookContextModelSnapshot : ModelSnapshot
+    [Migration("20250607033659_AddCategoryForNews")]
+    partial class AddCategoryForNews
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
